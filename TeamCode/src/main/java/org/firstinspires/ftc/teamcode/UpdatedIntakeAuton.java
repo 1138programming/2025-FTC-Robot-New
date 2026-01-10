e9d55f04679f0400b3be484e45a3c199a1bec5a4
JAKlein26 <117413359+JAKlein26@users.noreply.github.com>
2026-01-09 16:46:41 -0800

    auton updates + code cleanu

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UpdatedIntakeAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UpdatedIntakeAuton.java
@@ -69,8 +69,8 @@ public class UpdatedIntakeAuton extends LinearOpMode {
         flywheelMotor.setPower(flyWheelVelocity);
         waitms(4000);
         drivebase.driveTime(0, 0, 0.75f, false, 0.5f, 720, this);
-        horizontalLeft(550);
-        drivebase.driveTime(0, 1, 0, false, 0.5f, 1000, this);
+        horizontalLeft(800);
+        drivebase.driveTime(0, 1, 0, false, 0.5f, 2000, this);
         intakeMotor.setPower(-1);
         waitms(4000);
 
