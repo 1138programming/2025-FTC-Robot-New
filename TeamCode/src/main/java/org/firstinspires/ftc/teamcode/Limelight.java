1ec94f0a03b31c2300709fb875ee4ecc4f6f5a18
Justin-Zhang2029 <jzhang29@chaminet.org>
2026-01-30 19:46:33 -0800

    started ll

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Limelight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Limelight.java
@@ -1,7 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
+import java.util.List;
 import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes.*;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+
 
 public class Limelight {
     Limelight3A limelight;
@@ -15,6 +20,28 @@ public class Limelight {
     public double getTx(){
         LLResult result = limelight.getLatestResult();
         return result.getTx();
+    }
+
+    public double getTy(){
+        LLResult result = limelight.getLatestResult();
+        return result.getTy();
+    }
+    public int getTid(){
+        LLResult result = limelight.getLatestResult();
+        List<DetectorResult> detectorResults = result.getDetectorResults();
+
+        if (!detectorResults.isEmpty()){
+            return detectorResults.get(0).getClassId();
+        }
+        return 0;
+    }
+
+    public void setBotOrientation(double yaw){
+        limelight.updateRobotOrientation(yaw);
+    }
+
+    public Pose3D getBotPose(){
+        return limelight.getLatestResult().getBotpose_MT2();
     }
 
 
